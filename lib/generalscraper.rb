d4a43904d0c73d15f349e322f8c360b2247c404a
GH <shidash@shidash.com>
2015-04-05 10:07:35 +0200

    Made some changes, list of things to fix

--- lib/generalscraper.rb
+++ lib/generalscraper.rb
@@ -5,15 +5,28 @@ require 'open-uri'
 require 'uploadconvert'
 
 class GeneralScraper
-  def initialize(scrapesite, input, table, type)
+  def initialize(scrapesite, input, type)
    @input = input
    @scrapesite = scrapesite
    @output = Array.new
+   @urllist = Array.new
    @startindex = 10
-   @table = table
    @type = type
   end
 
+  # TODO:
+  # Get proxies working
+  # Choose one at random from list (list external)
+  # Remove delay
+  
+  # Script for running Google scraper
+
+  # Separate:
+  # Allow multiple operators and values
+  # Get page not in examine
+  # Separate file for page parsing
+  # Start agent/get URL
+  
   # Searches for links on Google
   def search
     agent = Mechanize.new
@@ -22,6 +35,16 @@ class GeneralScraper
     gform.q = @type+":" + @scrapesite + " " + @input
     page = agent.submit(gform, gform.buttons.first)
     examine(page)
+  end
+
+  # Generates query
+  def parseQuery
+    # Check if type list is array
+    # If it is not an array, process as @type+":"+@value+" " @input
+    # Otherwise set query to "" and loop through type and set-
+    # query += t+":"+@value[i]
+    # Keep index i
+    # Then at end- query += " "+@input
   end
  
   # Examines a search page
@@ -32,7 +55,8 @@ class GeneralScraper
         
         if saveurl[1]
           url = saveurl[1].split("&")
-          getPage(url[0])
+          @urllist.push(url[0])
+          # getPage(url[0])
         end
       end
 
@@ -43,7 +67,7 @@ class GeneralScraper
         if url2[0].to_i == @startindex
           sleep(rand(30..90))
           @startindex += 10
-          agent = Mechanize.new
+          agent = Mechanize.new # Need to modify this and split out
           examine(agent.get("http://google.com" + link.href + "&filter=0"))
         end
       end
@@ -65,16 +89,16 @@ class GeneralScraper
           pagehash[m['name']] = m['content']
         end
       end
-      if @table == false
-        if url.include? ".pdf"
-          `wget -P public/uploads #{url}`
-          path = url.split("/")
-          u = UploadConvert.new("public/uploads/" + path[path.length-1].chomp.strip)
-          pdfparse = JSON.parse(u.handleDoc)
-          pdfparse.each{|k, v| pagehash[k] = v}
-        else
-          pagehash[:text] = html.css("body").text
-        end
+
+      # Download and OCR any PDFs
+      if url.include? ".pdf"
+        `wget -P public/uploads #{url}`
+        path = url.split("/")
+        u = UploadConvert.new("public/uploads/" + path[path.length-1].chomp.strip)
+        pdfparse = JSON.parse(u.handleDoc)
+        pdfparse.each{|k, v| pagehash[k] = v}
+      else
+        pagehash[:text] = html.css("body").text
       end
       @output.push(pagehash)
     rescue
@@ -87,5 +111,13 @@ class GeneralScraper
     search
     return JSON.pretty_generate(@output)
   end
+
+  # Returns a list of search result URLs
+  def getURLs
+    search
+    return JSON.pretty_generate(@urllist)
+  end
 end
 
+g = GeneralScraper.new("linkedin.com/pub", "xkeyscore", "site")
+puts g.getURLs
