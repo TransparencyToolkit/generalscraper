f7e5bc6afd281f97b0f84375c4de22f3385d60e5
GH <shidash@shidash.com>
2015-04-06 20:37:14 +0200

    Refactored examine Can take multiple search operators

--- lib/generalscraper.rb
+++ lib/generalscraper.rb
@@ -5,74 +5,67 @@ require 'open-uri'
 require 'uploadconvert'
 
 class GeneralScraper
-  def initialize(scrapesite, input, type)
-   @input = input
-   @scrapesite = scrapesite
-   @output = Array.new
-   @urllist = Array.new
-   @startindex = 10
-   @type = type
+  def initialize(operators, searchterm)
+    @operators = operators
+    @searchterm = searchterm
+    @op_val = @operators.split(" ")[0].split(":")[1]
+    
+    @output = Array.new
+    @urllist = Array.new
+    @startindex = 10
   end
 
   # TODO:
   # Get proxies working
   # Choose one at random from list (list external)
   # Remove delay
-  
   # Script for running Google scraper
 
   # Separate:
-  # Allow multiple operators and values
   # Get page not in examine
   # Separate file for page parsing
-  # Start agent/get URL
+  # Start agent/get URL method
   
   # Searches for links on Google
   def search
     agent = Mechanize.new
     agent.user_agent_alias = 'Linux Firefox'
     gform = agent.get("http://google.com").form("f")
-    gform.q = @type+":" + @scrapesite + " " + @input
+    gform.q = @operators + " " + @searchterm
     page = agent.submit(gform, gform.buttons.first)
-    examine(page)
+    categorizeLinks(page)
   end
 
-  # Generates query
-  def parseQuery
-    # Check if type list is array
-    # If it is not an array, process as @type+":"+@value+" " @input
-    # Otherwise set query to "" and loop through type and set-
-    # query += t+":"+@value[i]
-    # Keep index i
-    # Then at end- query += " "+@input
-  end
- 
-  # Examines a search page
-  def examine(page)
+  # Categorizes the links on results page into results and other search pages
+  def categorizeLinks(page)
     page.links.each do |link|
-      if (link.href.include? @scrapesite) && (!link.href.include? "webcache") && (!link.href.include? @type+":"+@scrapesite)
-        saveurl = link.href.split("?q=")
-        
-        if saveurl[1]
-          url = saveurl[1].split("&")
-          @urllist.push(url[0])
-          # getPage(url[0])
-        end
-      end
-
-      if (link.href.include? "&sa=N") && (link.href.include? "&start=")
-        url1 = link.href.split("&start=")
-        url2 = url1[1].split("&sa=N")
-
-        if url2[0].to_i == @startindex
-          sleep(rand(30..90))
-          @startindex += 10
-          agent = Mechanize.new # Need to modify this and split out
-          examine(agent.get("http://google.com" + link.href + "&filter=0"))
-        end
+      if (link.href.include? @op_val) && (!link.href.include? "webcache") && (!link.href.include? @operators.gsub(" ", "+"))
+        siteURLSave(link)
+      elsif (link.href.include? "&sa=N") && (link.href.include? "&start=")
+        nextSearchPage(link)
       end
     end
   end
+
+  # Parse and save the URLs for search results
+  def siteURLSave(link)
+    site_url = link.href.split("?q=")[1]
+    @urllist.push(site_url.split("&")[0]) if site_url
+    # getPage(url[0]) handle this elsewhere
+  end
+
+  # Process search links and go to next page
+  def nextSearchPage(link)
+    page_index_num = link.href.split("&start=")[1].split("&sa=N")[0]
+    
+    if page_index_num.to_i == @startindex
+      sleep(rand(30..90)) # Eventually remove
+      @startindex += 10
+      agent = Mechanize.new # Need to modify this and split out
+      categorizeLinks(agent.get("http://google.com" + link.href + "&filter=0"))
+    end
+  end
+  
 
   # Scrape the page content
   def getPage(url)
@@ -119,5 +112,5 @@ class GeneralScraper
   end
 end
 
-g = GeneralScraper.new("linkedin.com/pub", "xkeyscore", "site")
+g = GeneralScraper.new("site:nsa.gov inurl:pdf", "cybersecurity science")
 puts g.getURLs
